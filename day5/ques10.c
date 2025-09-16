0f2377215559c91d24821301108f35495183bdc4
zeditya <adityabahuguna098@gmail.com>
2025-09-16 12:02:49 +0530

    day 10 completed

---
+++ day5/ques10.c
@@ -0,0 +1,31 @@
+// Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+// /*
+// Sample Test Cases:
+// Input 1:
+// 3661
+// Output 1:
+// 1:1:1
+
+// Input 2:
+// 7322
+// Output 2:
+// 2:2:2
+
+// */
+
+#include <stdio.h>
+int main () {
+  int tot_secs, hours , minutes , seconds;
+  printf("enter the time in seconds:\n");
+  scanf("%d", &tot_secs);
+  hours= tot_secs/3600;
+  int remaining_secs = tot_secs % 3600;
+  minutes = remaining_secs / 60;
+  seconds = remaining_secs % 60;
+
+  printf("%d:%d:%d\n", hours , minutes , seconds);
+
+
+  return 0;
+}
---
+++ day5/ques9.c
@@ -0,0 +1,39 @@
+// Q9: Write a program to calculate simple and compound interest for given principal, rate, and time.
+
+// /*
+// Sample Test Cases:
+// Input 1:
+// 1000 5 2
+// Output 1:
+// Simple Interest=100, Compound Interest=102.5
+
+// Input 2:
+// 5000 7 3
+// Output 2:
+// Simple Interest=1050, Compound Interest=1125.76
+
+// */
+
+
+#include <stdio.h>
+#include <math.h>
+int main (){
+  float princ,rate,time,si,ci,amount;
+  printf("enter the principal:\n");
+  scanf("%f", &princ);
+  printf("enter the rate:\n");
+  scanf("%f", &rate);
+  printf("enter the time:\n");
+  scanf("%f", &time);
+
+  si= princ*rate*time/100;
+
+  amount = princ * pow((1+rate/100), time);
+  ci= amount - princ;
+
+
+  printf("the simple interest is %.2f\n", si);
+  printf("the compound interest is %.2f\n", ci);
+
+  return 0;
+}
